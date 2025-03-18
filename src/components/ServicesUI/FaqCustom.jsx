f55017fc0f1e99786f5bb6598125457a6532be23
Genious-Bajracharya <102010683+Genious-Bajracharya@users.noreply.github.com>
2025-03-18 11:32:05 +0545

    updated code structure

--- src/components/ServicesUI/FaqCustom.jsx
+++ src/components/ServicesUI/FaqCustom.jsx
@@ -36,84 +36,65 @@ function FaqCustom() {
         },1000)
     },[isSuccess])
   return (
-    <div
-    className="rounded-lg"
-    >
-      <div
-      className="flex flex-col items-center justify-center rounded-lg "
-      >
-      <div
-      className="w-12 h-12 relative -mb-6 shadow-xl  z-10  bg-white rounded-full"
-      >
-        <img src="/ourservices/profile.svg"
-        className=" w-full h-full object-cover z-10 absolute"
-        />
-      </div>
-      <div
-      className=" relative w-full px-4 md:px-20 lg:px-72 py-12 bg-[#00264F]  text-white"
-      >
-       <form 
-       onSubmit={handleSubmit}
-      className="flex flex-col gap-3 lg:gap-8"
-       >
-       <label htmlFor="faq"
-        className="block text-xl w-full text-center "
-       
-        >Ask me a question</label>
-        <textarea
-        
-        name="faq" id="faq"
-        className=" w-full text-black px-2 py-1 outline-none rounded-md h-32"
-        value={formdata.faq}
-        placeholder="Drop your question here !!!"
-        onChange={handleChange}
-        required
-        rows={5}
-        minLength={5}
-        ></textarea>
-       
-       
-        
-        <div
-        className="flex flex-col md:flex-row gap-4"
-        >
-        <input
-        placeholder="Full Name"
-        type="text" 
-        className=" p-2 w-full text-black outline-none rounded-md"
-        name="name"
-        value={formdata.name}
-        onChange={handleChange}
-        minLength={3}
-        required
-        />
-        
-        <input
-        placeholder="Email"
-        type="email" name="email"
-        className=" w-full text-black p-2 outline-none rounded-md"
-        value={formdata.email}
-        onChange={handleChange}
-        required
-        />
-        <input
-        placeholder="Contact Number"
-        type="tel" name="phone"
-        className=" w-full text-black p-2 outline-none rounded-md"
-        pattern="^[0-9+/-/s()]*$"
-        value={formdata.phone}
-        onChange={handleChange}
-        required
-        />
+    <div className="rounded-lg">
+      <div className="flex flex-col items-center justify-center rounded-lg ">
+        <div className="w-12 h-12 relative -mb-6 shadow-xl  z-10  bg-white rounded-full">
+          <img src="/ourservices/profile.svg" className=" w-full h-full object-cover z-10 absolute"/>
         </div>
-        <button type="submit"
-        className="mx-auto w-fit px-12 border-2 mt-28  py-2 font-bold hover:bg-gray-200 hover:text-black text-gtay-100 rounded-lg"
-        >Submit</button>
-       </form>
-           {isSuccess &&  <p 
-           className="absolute bottom-32  right-56 lg:left-1/2 text-lg w-64 font-semibold rounded-lg bg-green-200 p-2 text-black text-center"
-           >Successfully sent</p>}
-      </div>
+        <div className=" relative w-full px-4 md:px-20 lg:px-72 py-12 bg-[#00264F]  text-white">
+        <form 
+          onSubmit={handleSubmit} className="flex flex-col gap-3 lg:gap-8">
+          <label htmlFor="faq" className="block text-xl w-full text-center "
+        >Ask me a question</label>
+          <textarea
+          name="faq" id="faq"
+          className=" w-full text-black px-2 py-1 outline-none rounded-md h-32"
+          value={formdata.faq}
+          placeholder="Drop your question here !!!"
+          onChange={handleChange}
+          required
+          rows={5}
+          minLength={5}
+          />
+          <div className="flex flex-col md:flex-row gap-4">
+            <input
+              placeholder="Full Name"
+              type="text" 
+              className=" p-2 w-full text-black outline-none rounded-md"
+              name="name"
+              value={formdata.name}
+              onChange={handleChange}
+              minLength={3}
+              required
+            />
+            
+            <input
+              placeholder="Email"
+              type="email" name="email"
+              className=" w-full text-black p-2 outline-none rounded-md"
+              value={formdata.email}
+              onChange={handleChange}
+              required
+            />
+            <input
+              placeholder="Contact Number"
+              type="tel" name="phone"
+              className=" w-full text-black p-2 outline-none rounded-md"
+              pattern="^[0-9+/-/s()]*$"
+              value={formdata.phone}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <button type="submit"
+          className="mx-auto w-fit px-12 border-2 mt-28  py-2 font-bold hover:bg-gray-200 hover:text-black text-gtay-100 rounded-lg"
+          >Submit</button>
+        </form>
+          {isSuccess &&  
+          <p 
+          className="absolute bottom-32  right-56 lg:left-1/2 text-lg w-64 font-semibold rounded-lg bg-green-200 p-2 text-black text-center"
+          >Successfully sent</p>}
+        </div>
       </div>
     </div>
   )
