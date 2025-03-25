140c0bd6e8a5fc7c55fa70e88b0675637582dc73
Genious-Bajracharya <102010683+Genious-Bajracharya@users.noreply.github.com>
2025-03-25 15:10:07 +0545

    changes youtuber video

--- src/components/faq/FaqVideo.jsx
+++ src/components/faq/FaqVideo.jsx
@@ -7,7 +7,7 @@ const FaqVideo = () =>{
                 <iframe 
                     width="560" 
                     height="315" 
-                    src="https://www.youtube.com/embed/VomOzeE7Z-k?si=yjioQ_Bq2owavx4o&amp;start=68&autoplay=1&mute=1" 
+                    src="https://www.youtube.com/embed/aI4vpHeGPa4?si=79Wthutg05QfUjea&amp;autoplay=1&mute=1" 
                     title="YouTube video player" 
                     frameborder="0" 
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
