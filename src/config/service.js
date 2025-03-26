995fd0045f738a48da533b4625d257cadbbcf5a8
Genious-Bajracharya <102010683+Genious-Bajracharya@users.noreply.github.com>
2025-03-26 16:01:22 +0545

    changed sectors pictures

--- src/config/service.js
+++ src/config/service.js
@@ -1,7 +1,7 @@
 export const serviceArr=[
-    { title: "Hospitality", img: "/industryServed/Hospitality.png" },
+    { title: "Hospitality", img: "/industryServed/hospitality.jpg" },
     { title: "Security", img: "/industryServed/security.jpg" },
-    { title: "Driving", img: "/industryServed/driving.jpg" },
+    { title: "Driving", img: "/industryServed/driving.webp" },
     { title: "Warehouse", img: "/industryServed/warehouse.jpg" },
     { title: "Factories", img: "/industryServed/factory.jpg" },
     { title: "Telecommunication", img: "/industryServed/telecommunication.jpg" },
