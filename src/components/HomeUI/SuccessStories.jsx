14ad7fda73e67a42dcabc1cfb28ba5c8b8c154d6
Genious-Bajracharya <102010683+Genious-Bajracharya@users.noreply.github.com>
2025-03-30 12:14:11 +0545

    updated succes stoires iteratinfg from array

--- src/components/HomeUI/SuccessStories.jsx
+++ src/components/HomeUI/SuccessStories.jsx
@@ -14,30 +14,30 @@ const success = [
   {
     id:0,
     name:"Mikmar Lama",
-    image:"/images/HomeUI/success/user1",
+    image:"/HomeUI/success/user1.png",
     location:"Sindhupalchok, Nepal",
     quote:"With their extensive network and industry expertise, they opened doors to opportunities I never knew existed, propelling my career to new heights."
   },
   {
     id:1,
     name:"Umesh Khatri",
-    image:"/images/HomeUI/success/user1",
+    image:"/HomeUI/success/user2.png",
     location:"Ramechhap, Nepal",
-    quote:"With their extensive network and industry expertise, they opened doors to opportunities I never knew existed, propelling my career to new heights."
+    quote:"Thanks to their vast network and deep industry knowledge, they unlocked opportunities I never thought possible, significantly advancing my career."
   },
   {
     id:2,
     name:"Subash Thapa",
-    image:"/images/HomeUI/success/user1",
+    image:"/HomeUI/success/user3.png",
     location:"Nawalparasi, Nepal",
-    quote:"With their extensive network and industry expertise, they opened doors to opportunities I never knew existed, propelling my career to new heights."
+    quote:"Their extensive connections and profound understanding of the industry revealed new pathways for my professional growth, elevating my career to unprecedented levels."
   },
   {
     id:3,
     name:"Shyam Shrestha",
-    image:"/images/HomeUI/success/user1",
+    image:"/HomeUI/success/user4.png",
     location:"Dolakha, Nepal",
-    quote:"With their extensive network and industry expertise, they opened doors to opportunities I never knew existed, propelling my career to new heights."
+    quote:"Thanks to their extensive connections and deep industry expertise, they revealed a multitude of new opportunities, transforming my career trajectory and pushing me to new heights of success."
   },
 ]
 
@@ -84,8 +84,8 @@ const CustomSlider = () => {
   return (
     <>
       <div className="bg-[#00254F] mt-[124px] flex flex-col md:flex-row">
-      <div className=" pt-12 px-8 font-Poppins md:hidden">
-         <h1 className="text-[#F7F7F7] font-semibold text-[28px]">
+        <div className=" pt-12 px-8 font-Poppins md:hidden">
+          <h1 className="text-[#F7F7F7] font-semibold text-[28px]">
             Our Success Stories
           </h1> 
           <p className="text-[#F7F7F7] text-left tracking-tight md:w-[363px] pt-[16px]">
@@ -97,64 +97,25 @@ const CustomSlider = () => {
           {...settings}
           className="md:w-[60%] gap-5 overflow-hidden pt-12 md:pt-[87px] rounded-[20px] md:pb-[127px] md:mr-[20px]"
         >
-          <div className="slide01  w-[300px] md:w-[607px] border-[10px] border-[#00254F] py-12  md:h-[450px] rounded-[12px] bg-white  ">
-            <div className="img flex  justify-center md:pt-[60px]">
-              <img src={user1} alt="" />
-            </div>
-            <h1 className="text-center pt-3 text-[22px] text-[#2C2C2C] font-semibold">
-              Rome Vuyvt
-            </h1>
-            <h1 className="text-center text-sm md:text-md pt-3">Driver at Tyuv Ine / Poland </h1>
-            <p className="px-1  hidden sm:block   text-[#333333] pt-5 md:px-4 ">
-              With their extensive network and industry expertise, they opened
-              doors to opportunities I never knew existed, propelling my career
-              to new heights.
-            </p>
-          </div>
-          <div className="slide01  w-[300px] md:w-[607px] border-[10px] border-[#00254F] py-12  md:h-[450px] rounded-[12px] bg-white  ">
-            <div className="img flex  justify-center md:pt-[60px]">
-              <img src={user2} alt="" className="h-24 w-24 rounded-full object-cover" />
-            </div>
-            <h1 className="text-center pt-3 text-[22px] text-[#2C2C2C] font-semibold">
-              Wanda 
-            </h1>
-            <h1 className="text-center text-sm md:text-md pt-3">Driver at Tyuv Ine / Poland </h1>
-            <p className="px-1  hidden sm:block  text-[#333333] pt-5 md:px-4 ">
-              Thanks to their vast network and deep industry knowledge, 
-              they unlocked opportunities I never thought possible, significantly advancing my career.
-            </p>
-          </div>
-          <div className="slide01  w-[300px] md:w-[607px] border-[10px] border-[#00254F] py-12  md:h-[450px] rounded-[12px] bg-white  ">
-            <div className="img flex  justify-center md:pt-[60px]">
-              <img src={user3} alt="" className="h-24 w-24 rounded-full object-cover" />
-            </div>
-            <h1 className="text-center pt-3 text-[22px] text-[#2C2C2C] font-semibold">
-              Steven Grantt
-            </h1>
-            <h1 className="text-center text-sm md:text-md pt-3">Driver at Tyuv Ine / Poland </h1>
-            <p className="px-1  hidden sm:block  text-[#333333] pt-5 md:px-4 ">
-              Thanks to their extensive connections and deep industry expertise, they revealed a multitude of new 
-              opportunities, transforming my career trajectory and pushing me to new heights of success.
-            </p>
-          </div>
-          <div className="slide01  w-[300px] md:w-[607px] border-[10px] border-[#00254F] py-12  md:h-[450px] rounded-[12px] bg-white  ">
-            <div className="img flex  justify-center md:pt-[60px]">
-              <img src={user4} alt="" className="h-24 w-24 rounded-full object-cover"/>
-            </div>
-            <h1 className="text-center pt-3 text-[22px] text-[#2C2C2C] font-semibold">
-              Mark Spector
-            </h1>
-            <h1 className="text-center text-sm md:text-md pt-3">Driver at Tyuv Ine / Poland </h1>
-            <p className="px-1  hidden sm:block  text-[#333333] pt-5 md:px-4 ">
-              Their extensive connections and profound understanding of the industry revealed new pathways 
-              for my professional growth, elevating my career to unprecedented levels.
-            </p>
-          </div>
-          
-         
+          {
+            success.map((user,index) =>(
+              <div className="slide01  w-[300px] md:w-[607px] border-[10px] border-[#00254F] py-12  md:h-[450px] rounded-[12px] bg-white  ">
+                <div className="img flex  justify-center md:pt-[60px]">
+                  <img src={user.image} alt={user.name} className="h-24 w-24 rounded-full object-cover" />
+                </div>
+                <h1 className="text-center pt-3 text-[22px] text-[#2C2C2C] font-semibold">
+                {user.name}
+                </h1>
+                <h1 className="text-center text-sm md:text-md pt-3">{user.location} </h1>
+                <p className="px-1  hidden sm:block  text-[#333333] pt-5 md:px-4 ">
+                  {user.quote}
+                </p>
+              </div>
+            ))
+          }
         </Slider>
 
-       <div className="right_side hidden md:block pt-[154px] pl-[50px] font-Poppins">
+        <div className="right_side hidden md:block pt-[154px] pl-[50px] font-Poppins">
           <h1 className="text-[#F7F7F7] font-semibold text-[32px]">
             Our Success Stories
           </h1>
