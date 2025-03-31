5ab71da4126769810e35b4fdcb607daf171d2b04
Genious-Bajracharya <102010683+Genious-Bajracharya@users.noreply.github.com>
2025-03-31 17:50:17 +0545

    updated responsiveness of servies

--- src/components/HomeUI/OurServiceUpdated/OurServices.jsx
+++ src/components/HomeUI/OurServiceUpdated/OurServices.jsx
@@ -4,31 +4,31 @@ import { useNavigate } from "react-router-dom"
 //     const redirect= useNavigate()
 //   return (
 //     <div className="px-5 md:px-20 xl:px-36 mt-12" >
-//         <h2 className="text-2xl my-12 md:text-2xl text-[#2C2C2C] md:w-96 xl:text-nowrap font-semibold" >Our Services</h2>
+//         <h2 className="text-lg md:text-2xl my-12 md:text-lg md:text-2xl text-[#2C2C2C] md:w-96 xl:text-nowrap font-semibold" >Our Services</h2>
 //       <div className="grid grid-cols-2 gap-4" >
 //         <div className="relative h-60 md:h-96 rounded-lg shadow-md " onClick={()=> redirect('/services')} >
 //         <img   
 //           className="w-full h-full object-cover rounded-xl  brightness-75" 
 //           src="/ourservices/recruitment.jpg.webp"/>
-//         <p className="absolute bottom-1 md:bottom-2  left-0 mx-1 md:left-0 lg:px-6 py-1 px-1 md:py-3 text-white  md:text-xl lg:text-2xl tracking-tight  rounded-lg" >Expert Recruitment <sub className="text-xs md:text-sm" > GCC & Europe</sub> </p>
+//         <p className="absolute bottom-1 md:bottom-2  left-0 mx-1 md:left-0 lg:px-6 py-1 px-1 md:py-3 text-white  md:text-xl lg:text-lg md:text-2xl tracking-tight  rounded-lg" >Expert Recruitment <sub className="text-xs md:text-sm" > GCC & Europe</sub> </p>
 //         </div>
 //         <div className="relative h-60 md:h-96 rounded-lg shadow-md" onClick={()=> redirect("/services")} >
 //         <img 
 //           className="h-full w-full absolute object-cover rounded-xl brightness-75  "
 //           src="/ourservices/manpower-solutions-1.webp"/>
-//         <p className="absolute text-white py-1 px-2 md:px-6 md:py-3   md:text-xl lg:text-2xl  z-10 filter  bottom-1 md:bottom-2 left-0 mx-1 md:left-0 rounded-lg " >Manpower Solutions</p>
+//         <p className="absolute text-white py-1 px-2 md:px-6 md:py-3   md:text-xl lg:text-lg md:text-2xl  z-10 filter  bottom-1 md:bottom-2 left-0 mx-1 md:left-0 rounded-lg " >Manpower Solutions</p>
 //         </div>
 //         <div className=" relative   rounded-lg h-60 md:h-96 shadow-md" onClick={()=> redirect("/services")} >
 //         <img  
 //           className="h-full w-full absolute object-cover rounded-xl brightness-75" 
 //           src="/ourservices/Talent-Acquisition.jpg"  />
-//         <p className="absolute bottom-1  md:bottom-2 md:left-0 left-0 mx-1 md:px-6 md:py-3 px-2 py-1 md:text-xl lg:text-2xl  text-white rounded-lg">Talent Acquisition</p>
+//         <p className="absolute bottom-1  md:bottom-2 md:left-0 left-0 mx-1 md:px-6 md:py-3 px-2 py-1 md:text-xl lg:text-lg md:text-2xl  text-white rounded-lg">Talent Acquisition</p>
 //         </div>
 //         <div className="relative h-60 md:h-96 rounded-lg shadow-md" onClick={()=> redirect("/services")} >
 //         <img  
 //           className="object-cover h-full w-full absolute rounded-xl brightness-75" 
 //           src="/HomeUI/consultant1.jpg"  />
-//         <p className="absolute z-10 text-white bottom-1 md:bottom-2 left-0 mx-1 md:left-0 md:px-6 px-2 py-1 md:py-3 md:text-xl lg:text-2xl rounded-lg" >Consulting Services</p>
+//         <p className="absolute z-10 text-white bottom-1 md:bottom-2 left-0 mx-1 md:left-0 md:px-6 px-2 py-1 md:py-3 md:text-xl lg:text-lg md:text-2xl rounded-lg" >Consulting Services</p>
 //         </div>
 //       </div>
 //     </div>
@@ -54,8 +54,8 @@ const OurServicesUpdated = () =>{
                   alt="" 
                   className="w-full h-full object-cover "/>
           </div>
-          <p className="text-2xl text-[#00254F] pt-8 text-center font-meduim">EXPERT RECRUITMENT in GCC & Europe</p>
-          <p  className="text-gray-600">We connect top talent with leading employers through our specialized recruitment services.</p>
+          <p className="text-lg md:text-2xl text-[#00254F] pt-8 text-center font-meduim">EXPERT RECRUITMENT in GCC & Europe</p>
+          <p  className="text-gray-600 hidden sm:block">We connect top talent with leading employers through our specialized recruitment services.</p>
         </div>
         {/* Card1 */}
         <div className="space-y-6 p-4 shadow-lg md:h-[380px] hover:shadow-md rounded-lg " 
@@ -65,8 +65,8 @@ const OurServicesUpdated = () =>{
                   alt="" 
                   className="w-full h-full object-cover "/>
           </div>
-          <p className="text-2xl text-[#00254F] pt-8 font-meduim text-center">CONSULTING SERVICES</p>
-          <p className="text-gray-600">Our expert consulting services optimize business performance and drive growth.</p>
+          <p className="text-lg md:text-2xl text-[#00254F] pt-8 font-meduim text-center">CONSULTING SERVICES</p>
+          <p className="text-gray-600 hidden sm:block">Our expert consulting services optimize business performance and drive growth.</p>
         </div>
         {/* Card1 */}
         <div className="space-y-6 p-4 shadow-lg md:h-[380px] hover:shadow-md rounded-lg " 
@@ -76,8 +76,8 @@ const OurServicesUpdated = () =>{
                   alt="" 
                   className="w-full h-full object-cover "/>
           </div>
-          <p className="text-2xl text-[#00254F] pt-8 font-meduim text-center">MANPOWER SOLUTIONS</p>
-          <p className="text-gray-600">We deliver customized workforce solutions to boost efficiency and productivity.</p>
+          <p className="text-lg md:text-2xl text-[#00254F] pt-8 font-meduim text-center">MANPOWER SOLUTIONS</p>
+          <p className="text-gray-600 hidden sm:block">We deliver customized workforce solutions to boost efficiency and productivity.</p>
         </div>
         {/* Card1 */}
         <div className="space-y-6 p-4 shadow-lg md:h-[380px] hover:shadow-md rounded-lg " 
@@ -87,8 +87,8 @@ const OurServicesUpdated = () =>{
                   alt="" 
                   className="w-full h-full object-cover  "/>
           </div>
-          <p className="text-2xl text-[#00254F] pt-8 font-meduim text-center">TALENT ACQUISITION</p>
-          <p className="text-gray-600">We expertly match top candidates with leading employers, ensuring the best fit.</p>
+          <p className="text-lg md:text-2xl text-[#00254F] pt-8 font-meduim text-center">TALENT ACQUISITION</p>
+          <p className="text-gray-600 hidden sm:block">We expertly match top candidates with leading employers, ensuring the best fit.</p>
         </div>
         
         
