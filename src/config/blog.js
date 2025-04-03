57ad4ede75c99504cccd133cc36d87c89bc14841
Genious-Bajracharya <102010683+Genious-Bajracharya@users.noreply.github.com>
2025-04-03 11:33:16 +0545

    added blogs array

--- src/config/blog.js
+++ src/config/blog.js
@@ -1,10 +1,26 @@
 export const Blogs=[
     {
         id:0,
-        title:"Important Update for Work Permit Applicants to Croatia",
-        
+        title:"IRCC confirms continuation of current express entry categories in 2024",
+        date:"January 12, 2024"
     },
     {
-
+        id:1,
+        title:"OET vs IELTS: which language test is right for you?",
+        date:"january 12, 2024"
     },
+    {
+        id:2,
+        title:"Important Update for Work Permit Applicants to Croatia",
+        date:"April 2, 2025"
+    },
+    {
+        id:3,
+        title:"Hungary Work Permit 2025: Latest updates for Nepali, Indian, and Filipino Citizens",
+        date:"April 4th 2025"
+    },
+    {
+        id:4,
+        title:"Shocking delays in Croatia Work Permit - Whats causing the hold-up?"
+    }
 ]
